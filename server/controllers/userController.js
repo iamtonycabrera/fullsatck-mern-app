4e627b15d86be4b323bde3e8b8e01819a340d43c
Antonio Cabrera <acabrerag94@gmail.com>
2023-08-18 16:25:14 +0200

    Implemented cancel booking method

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -47,3 +47,48 @@ export const bookVisit = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
+
+// get all users bookings
+export const allBookings = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  try {
+    const bookings = await prisma.user.findUnique({
+      where: { email },
+      select: { bookedVisits: true },
+    });
+    res.status(200).send(bookings);
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
+
+// cancel the booking
+export const cancelBooking = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: { bookedVisits: true },
+    });
+
+    const index = user.bookedVisits.findIndex((visit) => visit.id === id);
+
+    if (index === -1) {
+      res.status(404).json({ message: "Booking not found" });
+    } else {
+      user.bookedVisits.splice(index, 1);
+      await prisma.user.update({
+        where: { email },
+        data: {
+          bookedVisits: user.bookedVisits,
+        },
+      });
+      res.send("Booking cancelled successfully");
+    }
+  } catch (error) {
+    console.log("arror aqui");
+    throw new Error(error.message);
+  }
+});
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { bookVisit, createUser } from "../controllers/userController.js";
+import {
+  allBookings,
+  bookVisit,
+  cancelBooking,
+  createUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/bookVisit/:id", bookVisit);
-
-// TODO
 router.post("/allBookings", allBookings);
+router.post("/cancelBooking/:id", cancelBooking);
 
 export { router as userRoute };
